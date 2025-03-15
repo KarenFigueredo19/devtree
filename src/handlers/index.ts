19c44fc3cc460c358614e55676cb48fb0ccd7ae4
Karen Figueredo <karenbuitrago2519@gmail.com>
2025-03-14 22:32:45 -0500

    Avance parte 2 - Autenticación Usuarios - Hash contraseñas

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,20 @@
-import {Request, Response} from 'express'
+import { Request, Response } from "express";
 import User from "../models/user";
+import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
-    const user = new User(req.body);
-    await user.save();
-    res.send("Registro creado correctamente");
+
+  const { email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    const error = new Error("El usuario ya está registrado");
+    res.status(409).json({ error: error.message });
   }
+
+  const user = new User(req.body);
+  user.password = await hashPassword(password);
+  
+  await user.save();
+  res.status(201).send("Registro creado correctamente");
+};
