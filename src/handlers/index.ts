3714d0da48bc4485f65cdf995cce7da7e16be7f8
Karen Figueredo <karenbuitrago2519@gmail.com>
2025-03-17 17:07:11 -0500

    Avance 3 - Validaciones y Middleware

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,16 +2,10 @@ import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import slug from "slug";
 import User from "../models/user";
-import { hashPassword } from "../utils/auth";
+import { checkPassword, hashPassword } from "../utils/auth";
 
+/*Crear cuenta */
 export const createAccount = async (req: Request, res: Response) => {
-  //Manejar errores
-  let errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array });
-  }
-
   const { email, password } = req.body;
 
   const userExist = await User.findOne({ email });
@@ -33,4 +27,35 @@ export const createAccount = async (req: Request, res: Response) => {
 
   await user.save();
   res.status(201).send("Registro creado correctamente");
+};
+
+/*Iniciar sesión */
+export const login = async (req: Request, res: Response) => {
+  //Manejar errores
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    const error = new Error("El usuario no está registrado");
+    res.status(404).json({ error: error.message });
+  }
+
+  console.log("Sí existe...");
+
+  /*Comprobar password */
+  const isPasswordCorrect = await checkPassword(password, user.password);
+  console.log(user.password);
+  if (!isPasswordCorrect) {
+    const error = new Error("La contraseña es incorrecta");
+    res.status(401).json({ error: error.message });
+    return;
+  }
+  res.send("Autenticado...");
 };
--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,28 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { createAccount } from "./handlers";
+import { createAccount, login } from "./handlers";
+import { handleInputErrors } from "./middleware/validation";
 
 const router = Router();
 
 /*Autenticación y registro*/
 router.post(
   "/auth/register",
-  body("handle").notEmpty()
-  .withMessage("El handle no puede ir vacío"),
-  body("name").notEmpty()
-  .withMessage("El nombre no puede ir vacío"),
-  body("email").isEmail()
-  .withMessage("El Email no es válido"),
-  body("password").isLength({min: 8})
-  .withMessage("La contraseña es muy corto, debe ser mínimo 8 caracteres"),
+  body("handle").notEmpty().withMessage("El handle no puede ir vacío"),
+  body("name").notEmpty().withMessage("El nombre no puede ir vacío"),
+  body("email").isEmail().withMessage("El Email no es válido"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña es muy corta, debe ser mínimo 8 caracteres"),
+  handleInputErrors,
   createAccount
+);
+
+router.post(
+  "/auth/login",
+  body("email").isEmail().withMessage("El Email no es válido"),
+  body("password").notEmpty().withMessage("La contraseña es obligatoria"),
+  login
 );
 
 export default router;
